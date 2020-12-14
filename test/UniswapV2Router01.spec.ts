ea44ac39ae2044723f4a8d30c46540341ccc64d3
Samori <esdeseace@gmail.com>
2020-12-14 11:42:49 +0700

    Update keccak

--- test/UniswapV2Router01.spec.ts
+++ test/UniswapV2Router01.spec.ts
@@ -19,7 +19,7 @@ enum RouterVersion {
   UniswapV2Router02 = 'UniswapV2Router02'
 }
 
-describe('UniswapV2Router{01,02}', () => {
+describe('AnonyswapRouter{01,02}', () => {
   for (const routerVersion of Object.keys(RouterVersion)) {
     const provider = new MockProvider({
       hardfork: 'istanbul',
